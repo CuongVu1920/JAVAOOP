12d606a4c4d55ddd5bb78ec0afc915e154d0835d
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2024-01-26 08:17:10 +0700

    Create Project Lab2

---
+++ Lab2/BT3Trang93.java
@@ -0,0 +1,25 @@
+ package Lab2;
+
+import java.util.Scanner;
+
+class BT3Trang93 {
+
+      public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap ten: ");
+        String ten = sc.nextLine();
+        System.out.println("nhap tuoi: ");
+        int tuoi = sc.nextInt();
+
+        if(tuoi <16){
+            System.out.println("ban" + ten + " o do tuoi vi thanh nien!! ");
+        }else if(tuoi >= 16 && tuoi <= 18){
+            System.out.println("ban " + ten + " o do tuoi truong thanh!! ");
+        }else{
+            System.out.println("ban " + ten + " da gia");
+        }
+
+        sc.close();
+        
+    }
+}
---
+++ Lab2/BT5Trang93.java
@@ -0,0 +1,20 @@
+package Lab2;
+
+import java.util.Scanner;
+
+public class BT5Trang93 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       
+        int n;
+        int sum =0;
+        do{
+            System.out.println("nhap so n: ");
+            n = sc.nextInt();
+            sum +=n;
+        }while(sum<100);
+        System.out.println("tong cac so nguyen vua nhap: " + sum);
+        sc.close();
+    }
+}

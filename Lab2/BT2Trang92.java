84ce6dcf54f7260686aad1ba3e28b17266b61d58
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2024-02-02 22:22:34 +0700

    Lab2

---
+++ Lab2/BT2Trang92.java
@@ -0,0 +1,20 @@
+package Lab2;
+
+import java.util.Scanner;
+
+public class BT2Trang92 {
+    public static void main(String[] args) {
+          Scanner sc =new Scanner(System.in);
+        int n;
+        do{
+            System.out.println("Nhap 1 so nguyen duong ");
+            n = sc.nextInt();
+        }while(n<=0);
+        if(n%2==0){
+            System.out.println("So "+n+" la so chan ");
+        }else{
+            System.out.println("So "+n+" la so le ");
+        }
+        sc.close();
+    }
+}

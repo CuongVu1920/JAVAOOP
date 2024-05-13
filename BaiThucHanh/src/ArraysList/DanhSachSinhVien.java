6389dd65d22b0d1730fa747998eda073ac1d2d2a
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2024-05-13 13:53:48 +0700

    Bai Thuc Hanh 8/5

---
+++ BaiThucHanh/src/ArraysList/DanhSachSinhVien.java
@@ -0,0 +1,77 @@
+package ArraysList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class DanhSachSinhVien {
+    private ArrayList<SinhVien> danhSach;
+
+    public DanhSachSinhVien(){
+        this.danhSach = new ArrayList<SinhVien>();
+    }
+
+
+    public DanhSachSinhVien(ArrayList<SinhVien> danhSach) {
+        this.danhSach = danhSach;
+    }
+
+    //1. Them danh sach sinh vien
+    public void themSinhVien(SinhVien sv){
+        this.danhSach.add(sv);
+    }
+    //2. In danh sach sinh vien
+    public void inDanhSachSinhVien(){
+        for (SinhVien sinhVien : danhSach) {
+            System.out.println(sinhVien);
+        }
+    }
+
+    //3. Kiem tra sinh vien co rong hay khong
+    public boolean kiemTraDanhSachRong(){
+       return this.danhSach.isEmpty();
+    }
+    //4. lay ra so luong sinh vien trong danh sach
+    public int laySoLuongSinhVien(){
+        return this.danhSach.size();
+    }
+    //5. lam rong danh sach sinh vien
+    public void lamRongDanhSach(){
+        this.danhSach.removeAll(danhSach);
+    }
+    //6. Kiem tra sinh vien co ton tai trong danh sach hay khong, dua tren ma sinh vien.
+    public boolean kiemTraTonTai(SinhVien sv){
+        return this.danhSach.contains(sv);
+    }
+    //7. Xoa mot sinh vien ra khoi danh sach dua tren ma sinh vien
+    public boolean xoaSinhVien(SinhVien sv){
+        return this.danhSach.remove(sv);
+    }
+    //8. Tim kiem tat ca sinh vien dua tren Ten duoc nhap tu ban phim
+    public void timSinhVien(String ten){
+        for (SinhVien sinhVien : danhSach) {
+            if(sinhVien.getHoVaTen().indexOf(ten) >=0){
+                System.out.println(sinhVien);
+            }
+        }
+    }
+
+    //9. Xuat danh sach sinh vien co diem tu cao den thap
+    public void sapXepSinhVienGiamDanTheoDiem(){
+        Collections.sort(this.danhSach, new Comparator<SinhVien>() {
+
+            @Override
+            public int compare(SinhVien sv1, SinhVien sv2) {
+                if(sv1.getDiemTrungBinh() < sv2.getDiemTrungBinh()){
+                    return 1;
+                }else if(sv1.getDiemTrungBinh() > sv2.getDiemTrungBinh()){
+                    return -1;
+                }else{
+                    return 0;
+                }
+            }
+            
+            
+        } );
+    }
+}

18705f4cd82e5bcf1773dba6d0c2851ac4cb8b7b
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2024-05-13 15:01:20 +0700

    Bai Thuc Hanh 8/5

--- BaiThucHanh/src/ArraysList/SinhVien.java
+++ BaiThucHanh/src/ArraysList/SinhVien.java
@@ -47,4 +47,28 @@ public class SinhVien  {
         return "SinhVien [maSinhVien=" + maSinhVien + ", hoVaTen=" + hoVaTen + ", namSinh=" + namSinh
                 + ", diemTrungBinh=" + diemTrungBinh + "]";
     }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((maSinhVien == null) ? 0 : maSinhVien.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        SinhVien other = (SinhVien) obj;
+        if (maSinhVien == null) {
+            if (other.maSinhVien != null)
+                return false;
+        } else if (!maSinhVien.equals(other.maSinhVien))
+            return false;
+        return true;
+    }
+    
    }

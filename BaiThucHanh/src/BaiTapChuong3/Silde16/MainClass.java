c18017c9d700c082f43085be87935d998d9624c2
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2024-05-14 22:58:19 +0700

    Bai Thuc Hanh Chuong 3 - Silde 16

---
+++ BaiThucHanh/src/BaiTapChuong3/Silde16/MainClass.java
@@ -0,0 +1,27 @@
+package BaiTapChuong3.Silde16;
+
+import Person.Student;
+
+public class MainClass {
+    public static void main(String[] args) {
+       Employee nv = new Employee();
+       nv.nhap();
+       nv.in();
+       Person ps = new Person();
+       ps.in();
+       Student st = new Student();
+       st.nhap();
+       st.tinhTuoi();
+       st.in();
+       HinhChuNhat hcn =new HinhChuNhat();
+       hcn.nhap(2, 5);
+       hcn.tinhtoan();  
+       hcn.in();
+       HinhTron ht = new HinhTron();
+       ht.nhap();
+       ht.tinhChuVi();
+       ht.tinhdienTich();
+       ht.in();
+    }
+
+}

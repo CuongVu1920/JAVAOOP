b5274c898205c02d2bd36adf25646c127bc0d3db
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2024-05-17 21:52:48 +0700

    Bai Thuc Hanh Chuong 4 - Silde 57 - 17/5

---
+++ BaiThucHanh/src/BaiTapChuong4/Set/Hash.java
@@ -0,0 +1,37 @@
+package BaiTapChuong4.Set;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Hash {
+    public static void main(String[] args) {
+        HashSet<String> hashSetString = new HashSet<String>();
+        Scanner sc = new Scanner(System.in);
+
+        //them phan tu vao hashSetString
+        hashSetString.add("Wilson");
+        hashSetString.add("Nike");
+        hashSetString.add("Volvo");
+        hashSetString.add("Kia");
+        hashSetString.add("Lenovo");
+        hashSetString.add("Lenovo");
+
+        //hien thi cac phan tu trong hashSetString
+        System.out.println("Cac phan tu trong hashSetString: "); 
+        System.out.println(hashSetString);
+        System.out.println("Nhap phan tu can xoa: ");
+        String name;
+        name = sc.nextLine();
+        if(hashSetString.contains(name)){
+            hashSetString.remove(name);
+            System.out.println("Xoa thanh cong");
+            System.out.println("Cac phan tu con lai trong hashSetString: ");
+            System.out.println(hashSetString);
+        }else{
+            System.out.println("Xoa khong thanh cong!");
+        }
+
+
+    }
+
+}

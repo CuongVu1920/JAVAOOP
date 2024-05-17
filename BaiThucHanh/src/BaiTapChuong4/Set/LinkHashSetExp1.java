70e0ac63b393e2c3a4243af35776a4ad6f4739f9
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2024-05-17 22:05:57 +0700

    Bai Thuc Hanh Chuong 4 - Silde 60 - 17/5

---
+++ BaiThucHanh/src/BaiTapChuong4/Set/LinkHashSetExp1.java
@@ -0,0 +1,34 @@
+package BaiTapChuong4.Set;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class LinkHashSetExp1 {
+    public static void main(String[] args) {
+        //them cac phan tu
+        Set<String> linkedHashSet = new LinkedHashSet<String>();
+        linkedHashSet.add("Java");
+        linkedHashSet.add("C++");
+        linkedHashSet.add("Java");
+        linkedHashSet.add("PHP");
+        System.out.println("Cac phan tu trong linkedHashSet: ");
+        //hien thi set
+        for (String str : linkedHashSet) {
+            System.out.println(str);
+        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap phan tu can xoa");
+        String s = sc.nextLine();
+        if(linkedHashSet.contains(s)){
+            linkedHashSet.remove(s);
+            System.out.println("Xoa thanh cong!!");
+            System.out.println("linkedHashSet moi la: ");
+            for (String str : linkedHashSet) {
+                System.out.println(str);
+            }
+        }else{
+            System.out.println("Phan tu " + s + " khong co");
+        }
+    }
+}

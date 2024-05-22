f8a72288f46cc1e89528cc9e00e72931584f9479
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2024-05-22 09:35:35 +0700

    VVC792_Cau1

---
+++ BaiKiemTra/src/Person.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Person {
+    public String maSinhVien;
+    public String hoVaTen;
+    public double diem;
+    public Person(String maSinhVien, String hoVaTen, double diem) {
+        this.maSinhVien = maSinhVien;
+        this.hoVaTen = hoVaTen;
+        this.diem = diem;
+    }
+
+   
+}
